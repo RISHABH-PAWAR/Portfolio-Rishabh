c7c755a93f554709a21459e7485c449790b1b239
rishu <rishabhpawar401@gmail.com>
2025-12-18 14:36:05 +0530

    fix

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,9 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://8a1efbafa891faf7a8adb3f9ec8c8359@o4510495065178112.ingest.us.sentry.io/4510554612367360",
-
   tracesSampleRate: 1,
-
-  enableLogs: true,
   sendDefaultPii: true,
 });
